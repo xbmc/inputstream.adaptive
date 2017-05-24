ed92499b81dcc5daceb8c1e80cebaca4421307cd
peak3d <pfau@peak3d.de>
2017-05-24 14:05:47 +0200

    [DASH]Don't normalize seg::range_end at timelined templates (used to fill  placeholders)

--- src/parser/DASHTree.cpp
+++ src/parser/DASHTree.cpp
@@ -545,7 +545,7 @@ start(void *data, const char *el, const char **attr)
               attr += 2;
             }
             if(dash->current_adaptationset_->segment_durations_.data.empty())
-              dash->current_adaptationset_->startPTS_ = t-(dash->base_time_)*dash->current_adaptationset_->timescale_;
+              dash->current_adaptationset_->startPTS_ = t;
             if (d && r)
             {
               for (; r; --r)
@@ -976,7 +976,8 @@ end(void *data, const char *el)
                     dash->current_representation_->flags_ |= AdaptiveTree::Representation::TIMELINE;
 
                   seg.range_end_ = tpl.startNumber;
-                  seg.startPTS_ = seg.range_begin_ = dash->current_adaptationset_->startPTS_;
+                  seg.startPTS_ = dash->current_adaptationset_->startPTS_ - (dash->base_time_)*dash->current_adaptationset_->timescale_;
+                  seg.range_begin_ = dash->current_adaptationset_->startPTS_;
 
                   if (!timeBased && dash->available_time_ && dash->stream_start_ - dash->available_time_ > dash->overallSeconds_) //we need to adjust the start-segment
                     seg.range_end_ += static_cast<uint64_t>(((dash->stream_start_ - dash->available_time_ - dash->overallSeconds_)*tpl.timescale) / tpl.duration);
@@ -984,8 +985,8 @@ end(void *data, const char *el)
                   for (;countSegs;--countSegs)
                   {
                     dash->current_representation_->segments_.data.push_back(seg);
-                    seg.startPTS_ += (sdb != sde) ? *(sdb++) : tpl.duration;
-                    seg.range_begin_ = seg.startPTS_;
+                    uint32_t duration((sdb != sde) ? *(sdb++) : tpl.duration);
+                    seg.startPTS_ += duration, seg.range_begin_ += duration;
                     ++seg.range_end_;
                   }
                   return;
