59422e6904cae309cb36580ff0db96a8aaef6cb5
peak3d <pfau@peak3d.de>
2017-07-05 17:31:44 +0200

    Add secure decoder flag

--- src/SSD_dll.h
+++ src/SSD_dll.h
@@ -145,6 +145,7 @@ namespace SSD
       static const uint32_t SSD_ANNEXB_REQUIRED = 4;
       static const uint32_t SSD_HDCP_RESTRICTED = 8;
       static const uint32_t SSD_SINGLE_DECRYPT = 16;
+      static const uint32_t SSD_SECURE_DECODER = 32;
 
       static const uint32_t SSD_MEDIA_VIDEO = 1;
       static const uint32_t SSD_MEDIA_AUDIO = 2;
