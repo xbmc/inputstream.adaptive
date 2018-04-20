cd6d9c9884ae97fa6d31071ca4dff0b6004e51d6
peak3d <pfau@peak3d.de>
2018-04-20 19:51:53 +0200

    fix effective_url from 2.2.15 change

--- src/main.cpp
+++ src/main.cpp
@@ -277,11 +277,11 @@ bool adaptive::AdaptiveTree::download(const char* url, const std::map<std::strin
     effective_url_ = file.GetPropertyValue(ADDON_FILE_PROPERTY_EFFECTIVE_URL, "");
     kodi::Log(ADDON_LOG_DEBUG, "Effective URL %s", effective_url_.c_str());
 
-    std::string::size_type paramPos = effective_url_.find_first_of('?', effective_url_.length());
+    std::string::size_type paramPos = effective_url_.find_first_of('?');
     if (paramPos != std::string::npos)
       effective_url_.resize(paramPos);
 
-    paramPos = effective_url_.find_last_of('/', effective_url_.length());
+    paramPos = effective_url_.find_last_of('/');
     if (paramPos != std::string::npos)
       effective_url_.resize(paramPos + 1);
     else
