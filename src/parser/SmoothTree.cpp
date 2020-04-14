ca2fd36a3964ad8a831bf8cdc66468d8082d3122
peak3d <pfau@peak3d.de>
2020-04-14 22:06:25 +0200

    [ISM] Implement AnnexB to HVCC for ISM hevc codec extra data

--- src/parser/SmoothTree.cpp
+++ src/parser/SmoothTree.cpp
@@ -16,16 +16,17 @@
 *
 */
 
-#include <string>
-#include <cstring>
-#include <fstream>
-#include <float.h>
-#include <algorithm>
-
 #include "SmoothTree.h"
+
+#include "../helpers.h"
 #include "../oscompat.h"
 #include "PRProtectionParser.h"
-#include "../helpers.h"
+
+#include <algorithm>
+#include <cstring>
+#include <float.h>
+#include <fstream>
+#include <string>
 
 using namespace adaptive;
 
@@ -38,10 +39,9 @@ SmoothTree::SmoothTree()
 /*----------------------------------------------------------------------
 |   expat start
 +---------------------------------------------------------------------*/
-static void XMLCALL
-start(void *data, const char *el, const char **attr)
+static void XMLCALL start(void* data, const char* el, const char** attr)
 {
-  SmoothTree *dash(reinterpret_cast<SmoothTree*>(data));
+  SmoothTree* dash(reinterpret_cast<SmoothTree*>(data));
 
   if (dash->currentNode_ & SmoothTree::SSMNODE_SSM)
   {
@@ -53,11 +53,12 @@ start(void *data, const char *el, const char **attr)
         {
           if (strcmp((const char*)*attr, "SystemID") == 0)
           {
-            if (strstr((const char*)*(attr + 1), "9A04F079-9840-4286-AB92-E65BE0885F95")
-            || strstr((const char*)*(attr + 1), "9a04f079-9840-4286-ab92-e65be0885f95"))
+            if (strstr((const char*)*(attr + 1), "9A04F079-9840-4286-AB92-E65BE0885F95") ||
+                strstr((const char*)*(attr + 1), "9a04f079-9840-4286-ab92-e65be0885f95"))
             {
               dash->strXMLText_.clear();
-              dash->currentNode_ |= SmoothTree::SSMNODE_PROTECTIONHEADER| SmoothTree::SSMNODE_PROTECTIONTEXT;
+              dash->currentNode_ |=
+                  SmoothTree::SSMNODE_PROTECTIONHEADER | SmoothTree::SSMNODE_PROTECTIONTEXT;
             }
             break;
           }
@@ -78,7 +79,8 @@ start(void *data, const char *el, const char **attr)
         dash->current_representation_->flags_ |= AdaptiveTree::Representation::TEMPLATE;
         dash->current_representation_->segtpl_.media = dash->current_representation_->url_;
 
-        std::string::size_type pos(dash->current_representation_->segtpl_.media.find("{start time}"));
+        std::string::size_type pos(
+            dash->current_representation_->segtpl_.media.find("{start time}"));
         if (pos != std::string::npos)
           dash->current_representation_->segtpl_.media.replace(pos, 12, "$Time$");
         else
@@ -88,7 +90,8 @@ start(void *data, const char *el, const char **attr)
         if (pos == std::string::npos)
           return;
 
-        const char *bw = "0";
+        const char* bw = "0";
+        const char* codecPrivateData = nullptr;
 
         for (; *attr;)
         {
@@ -97,36 +100,64 @@ start(void *data, const char *el, const char **attr)
           else if (strcmp((const char*)*attr, "FourCC") == 0)
           {
             dash->current_representation_->codecs_ = (const char*)*(attr + 1);
-            std::transform(dash->current_representation_->codecs_.begin(), dash->current_representation_->codecs_.end(), dash->current_representation_->codecs_.begin(), ::tolower);
+            std::transform(dash->current_representation_->codecs_.begin(),
+                           dash->current_representation_->codecs_.end(),
+                           dash->current_representation_->codecs_.begin(), ::tolower);
           }
           else if (strcmp((const char*)*attr, "MaxWidth") == 0)
-            dash->current_representation_->width_ = static_cast<uint16_t>(atoi((const char*)*(attr + 1)));
+            dash->current_representation_->width_ =
+                static_cast<uint16_t>(atoi((const char*)*(attr + 1)));
           else if (strcmp((const char*)*attr, "MaxHeight") == 0)
-            dash->current_representation_->height_ = static_cast<uint16_t>(atoi((const char*)*(attr + 1)));
+            dash->current_representation_->height_ =
+                static_cast<uint16_t>(atoi((const char*)*(attr + 1)));
           else if (strcmp((const char*)*attr, "SamplingRate") == 0)
-            dash->current_representation_->samplingRate_ = static_cast<uint32_t>(atoi((const char*)*(attr + 1)));
+            dash->current_representation_->samplingRate_ =
+                static_cast<uint32_t>(atoi((const char*)*(attr + 1)));
           else if (strcmp((const char*)*attr, "Channels") == 0)
-            dash->current_representation_->channelCount_ = static_cast<uint8_t>(atoi((const char*)*(attr + 1)));
+            dash->current_representation_->channelCount_ =
+                static_cast<uint8_t>(atoi((const char*)*(attr + 1)));
           else if (strcmp((const char*)*attr, "Index") == 0)
             dash->current_representation_->id = (const char*)*(attr + 1);
           else if (strcmp((const char*)*attr, "CodecPrivateData") == 0)
-            dash->current_representation_->codec_private_data_ = annexb_to_avc((const char*)*(attr + 1));
+            codecPrivateData = (const char*)*(attr + 1);
           else if (strcmp((const char*)*attr, "NALUnitLengthField") == 0)
-            dash->current_representation_->nalLengthSize_ = static_cast<uint8_t>(atoi((const char*)*(attr + 1)));
+            dash->current_representation_->nalLengthSize_ =
+                static_cast<uint8_t>(atoi((const char*)*(attr + 1)));
           attr += 2;
         }
 
-        if (dash->current_representation_->codecs_ == "aacl" && dash->current_representation_->codec_private_data_.empty())
+        if (codecPrivateData)
+        {
+          if (dash->current_representation_->codecs_.compare(0, 3, "hev") == 0)
+            dash->current_representation_->codec_private_data_ = annexb_to_hvcc(codecPrivateData);
+          else
+            dash->current_representation_->codec_private_data_ = annexb_to_avc(codecPrivateData);
+        }
+
+        if (dash->current_representation_->codecs_ == "aacl" &&
+            dash->current_representation_->codec_private_data_.empty())
         {
           uint16_t esds(0x1010), sidx(4);
           switch (dash->current_representation_->samplingRate_)
           {
-            case 96000: sidx = 0; break;
-            case 88200: sidx = 1; break;
-            case 64000: sidx = 2; break;
-            case 48000: sidx = 3; break;
-            case 44100: sidx = 4; break;
-            case 32000: sidx = 5; break;
+            case 96000:
+              sidx = 0;
+              break;
+            case 88200:
+              sidx = 1;
+              break;
+            case 64000:
+              sidx = 2;
+              break;
+            case 48000:
+              sidx = 3;
+              break;
+            case 44100:
+              sidx = 4;
+              break;
+            case 32000:
+              sidx = 5;
+              break;
             default:;
           }
           esds |= (sidx << 7);
@@ -155,7 +186,8 @@ start(void *data, const char *el, const char **attr)
             {
               //Go back to the previous timestamp to calculate the real gap.
               dash->pts_helper_ -= dash->current_adaptationset_->segment_durations_.data.back();
-              dash->current_adaptationset_->segment_durations_.data.back() = static_cast<uint32_t>(lt - dash->pts_helper_);
+              dash->current_adaptationset_->segment_durations_.data.back() =
+                  static_cast<uint32_t>(lt - dash->pts_helper_);
             }
             else
               dash->current_adaptationset_->startPTS_ = lt;
@@ -189,23 +221,27 @@ start(void *data, const char *el, const char **attr)
       {
         if (strcmp((const char*)*attr, "Type") == 0)
           dash->current_adaptationset_->type_ =
-          stricmp((const char*)*(attr + 1), "video") == 0 ? SmoothTree::VIDEO
-          : stricmp((const char*)*(attr + 1), "audio") == 0 ? SmoothTree::AUDIO
-          : stricmp((const char*)*(attr + 1), "text") == 0 ? SmoothTree::SUBTITLE
-          : SmoothTree::NOTYPE;
+              stricmp((const char*)*(attr + 1), "video") == 0
+                  ? SmoothTree::VIDEO
+                  : stricmp((const char*)*(attr + 1), "audio") == 0
+                        ? SmoothTree::AUDIO
+                        : stricmp((const char*)*(attr + 1), "text") == 0 ? SmoothTree::SUBTITLE
+                                                                         : SmoothTree::NOTYPE;
         else if (strcmp((const char*)*attr, "Language") == 0)
           dash->current_adaptationset_->language_ = (const char*)*(attr + 1);
         else if (strcmp((const char*)*attr, "TimeScale") == 0)
           dash->current_adaptationset_->timescale_ = atoi((const char*)*(attr + 1));
         else if (strcmp((const char*)*attr, "Chunks") == 0)
-          dash->current_adaptationset_->segment_durations_.data.reserve(atoi((const char*)*(attr + 1)));
+          dash->current_adaptationset_->segment_durations_.data.reserve(
+              atoi((const char*)*(attr + 1)));
         else if (strcmp((const char*)*attr, "Url") == 0)
           dash->current_adaptationset_->base_url_ = dash->base_url_ + (const char*)*(attr + 1);
         attr += 2;
       }
       dash->segcount_ = 0;
       if (!dash->current_adaptationset_->timescale_)
-        dash->current_adaptationset_->timescale_ = 10000000; //Defalt Smmoothstream frequency (10Khz)
+        dash->current_adaptationset_->timescale_ =
+            10000000; //Defalt Smmoothstream frequency (10Khz)
 
 
       dash->currentNode_ |= SmoothTree::SSMNODE_STREAMINDEX;
@@ -215,7 +251,7 @@ start(void *data, const char *el, const char **attr)
       dash->currentNode_ |= SmoothTree::SSMNODE_PROTECTION;
       dash->current_period_->encryptionState_ = SmoothTree::ENCRYTIONSTATE_SUPPORTED;
       dash->current_period_->need_secure_decoder_ = true;
-	  }
+    }
   }
   else if (strcmp(el, "SmoothStreamingMedia") == 0)
   {
@@ -251,22 +287,20 @@ start(void *data, const char *el, const char **attr)
 /*----------------------------------------------------------------------
 |   expat text
 +---------------------------------------------------------------------*/
-static void XMLCALL
-text(void *data, const char *s, int len)
+static void XMLCALL text(void* data, const char* s, int len)
 {
-  SmoothTree *dash(reinterpret_cast<SmoothTree*>(data));
+  SmoothTree* dash(reinterpret_cast<SmoothTree*>(data));
 
-  if (dash->currentNode_  & SmoothTree::SSMNODE_PROTECTIONTEXT)
+  if (dash->currentNode_ & SmoothTree::SSMNODE_PROTECTIONTEXT)
     dash->strXMLText_ += std::string(s, len);
 }
 
 /*----------------------------------------------------------------------
 |   expat end
 +---------------------------------------------------------------------*/
-static void XMLCALL
-end(void *data, const char *el)
+static void XMLCALL end(void* data, const char* el)
 {
-  SmoothTree *dash(reinterpret_cast<SmoothTree*>(data));
+  SmoothTree* dash(reinterpret_cast<SmoothTree*>(data));
 
   if (dash->currentNode_ & SmoothTree::SSMNODE_SSM)
   {
@@ -279,7 +313,8 @@ end(void *data, const char *el)
       }
       else if (strcmp(el, "Protection") == 0)
       {
-        dash->currentNode_ &= ~(SmoothTree::SSMNODE_PROTECTION| SmoothTree::SSMNODE_PROTECTIONTEXT);
+        dash->currentNode_ &=
+            ~(SmoothTree::SSMNODE_PROTECTION | SmoothTree::SSMNODE_PROTECTIONTEXT);
         PRProtectionParser parser(dash->strXMLText_);
         dash->current_defaultKID_ = parser.getKID();
         dash->license_url_ = parser.getLicenseURL();
@@ -291,8 +326,8 @@ end(void *data, const char *el)
     {
       if (strcmp(el, "StreamIndex") == 0)
       {
-        if (dash->current_adaptationset_->representations_.empty()
-        || dash->current_adaptationset_->segment_durations_.data.empty())
+        if (dash->current_adaptationset_->representations_.empty() ||
+            dash->current_adaptationset_->segment_durations_.data.empty())
           dash->current_period_->adaptationSets_.pop_back();
         else
         {
@@ -308,12 +343,11 @@ end(void *data, const char *el)
 }
 
 
-
 /*----------------------------------------------------------------------
 |   SmoothTree
 +---------------------------------------------------------------------*/
 
-bool SmoothTree::open(const std::string &url, const std::string &manifestUpdateParam)
+bool SmoothTree::open(const std::string& url, const std::string& manifestUpdateParam)
 {
   PreparePaths(url, manifestUpdateParam);
 
@@ -339,15 +373,21 @@ bool SmoothTree::open(const std::string &url, const std::string &manifestUpdateP
   if (!current_defaultKID_.empty())
     psshset = static_cast<uint8_t>(insert_psshset(STREAM_TYPE_COUNT));
 
-  for (std::vector<AdaptationSet*>::iterator ba(current_period_->adaptationSets_.begin()), ea(current_period_->adaptationSets_.end()); ba != ea; ++ba)
+  for (std::vector<AdaptationSet*>::iterator ba(current_period_->adaptationSets_.begin()),
+       ea(current_period_->adaptationSets_.end());
+       ba != ea; ++ba)
   {
-    for (std::vector<SmoothTree::Representation*>::iterator b((*ba)->representations_.begin()), e((*ba)->representations_.end()); b != e; ++b)
+    for (std::vector<SmoothTree::Representation*>::iterator b((*ba)->representations_.begin()),
+         e((*ba)->representations_.end());
+         b != e; ++b)
     {
       (*b)->segments_.data.resize((*ba)->segment_durations_.data.size());
       std::vector<uint32_t>::iterator bsd((*ba)->segment_durations_.data.begin());
       uint64_t cummulated((*ba)->startPTS_ - base_time_), index(1);
 
-      for (std::vector<SmoothTree::Segment>::iterator bs((*b)->segments_.data.begin()), es((*b)->segments_.data.end()); bs != es; ++bsd, ++bs, ++index)
+      for (std::vector<SmoothTree::Segment>::iterator bs((*b)->segments_.data.begin()),
+           es((*b)->segments_.data.end());
+           bs != es; ++bsd, ++bs, ++index)
       {
         bs->startPTS_ = cummulated;
         bs->range_begin_ = cummulated + base_time_;
@@ -363,7 +403,7 @@ bool SmoothTree::open(const std::string &url, const std::string &manifestUpdateP
   return true;
 }
 
-bool SmoothTree::write_data(void *buffer, size_t buffer_size, void *opaque)
+bool SmoothTree::write_data(void* buffer, size_t buffer_size, void* opaque)
 {
   bool done(false);
   XML_Status retval = XML_Parse(parser_, (const char*)buffer, buffer_size, done);
