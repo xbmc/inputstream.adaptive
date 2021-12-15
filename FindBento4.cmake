eb7da4641d94824899d3168cb28ebae3510cb6c3
Alwin Esch <alwin.esch@web.de>
2021-12-15 03:23:29 +0100

    Crazy workaround

--- FindBento4.cmake
+++ FindBento4.cmake
@@ -9,20 +9,21 @@
 # BENTO4_INCLUDE_DIRS - the Bento4 include directory
 # BENTO4_LIBRARIES - the Bento4 libraries
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_BENTO4 REQUIRED)
-endif()
+execute_process(COMMAND ${CMAKE_COMMAND}
+    -G "${CMAKE_GENERATOR}"
+    -A "${CMAKE_GENERATOR_PLATFORM}"
+    -T "${CMAKE_GENERATOR_TOOLSET}"
+    ${CMAKE_SOURCE_DIR}/lib/libbento4
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
 
-find_path(BENTO4_INCLUDE_DIR NAMES bento4/Ap4.h PATHS ${PC_BENTO4_INCLUDEDIR})
+execute_process(COMMAND ${CMAKE_COMMAND}
+    --build .
+    --config ${CMAKE_BUILD_TYPE}
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
 
-find_library(BENTO4_LIBRARY NAMES ap4 PATHS ${PC_BENTO4_LIBDIR})
-                                   
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Bento4 REQUIRED_VARS BENTO4_LIBRARY BENTO4_INCLUDE_DIR)
+find_path(BENTO4_INCLUDE_DIR NAMES bento4/Ap4.h PATHS ${CMAKE_BINARY_DIR}/depends/include)
+find_library(BENTO4_LIBRARY NAMES ap4 PATHS ${CMAKE_BINARY_DIR}/depends/lib)
 
-if(BENTO4_FOUND)
-  set(BENTO4_LIBRARIES ${BENTO4_LIBRARY})
-  set(BENTO4_INCLUDE_DIRS ${BENTO4_INCLUDE_DIR})
-endif()
-
-mark_as_advanced(BENTO4_INCLUDE_DIR BENTO4_LIBRARY)
+set(BENTO4_FOUND TRUE)
+set(BENTO4_LIBRARIES ${BENTO4_LIBRARY})
+set(BENTO4_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/depends/include)
