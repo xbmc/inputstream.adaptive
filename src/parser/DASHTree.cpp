5e2213fce8d5606d9fc7586c1192e1bec659d8d2
CastagnaIT <gottardo.stefano.83@gmail.com>
2024-03-07 07:49:46 +0100

    Revert "[DashTree] Skip encrypted subtitle streams"
    This reverts commit eedc74f603b63c783e4724f0c99b6477e15423f5.

--- src/parser/DASHTree.cpp
+++ src/parser/DASHTree.cpp
@@ -627,13 +627,6 @@ void adaptive::CDashTree::ParseTagAdaptationSet(pugi::xml_node nodeAdp, PLAYLIST
   // Parse <ContentProtection> child tags
   if (nodeAdp.child("ContentProtection"))
   {
-    if (adpSet->GetStreamType() == StreamType::SUBTITLE)
-    {
-      LOG::LogF(LOGWARNING, "Skipped AdaptationSet with id: \"%s\", encrypted subtitles not supported.",
-                adpSet->GetId().data());
-      return;
-    }
-
     period->SetEncryptionState(EncryptionState::ENCRYPTED);
     std::vector<uint8_t> pssh;
     std::string kid;
@@ -970,14 +963,6 @@ void adaptive::CDashTree::ParseTagRepresentation(pugi::xml_node nodeRepr,
   // Parse <ContentProtection> tags
   if (nodeRepr.child("ContentProtection"))
   {
-    if (adpSet->GetStreamType() == StreamType::SUBTITLE)
-    {
-      LOG::LogF(LOGWARNING,
-                "Skipped Representation with id: \"%s\", encrypted subtitles not supported.",
-                repr->GetId().data());
-      return;
-    }
-
     period->SetEncryptionState(EncryptionState::ENCRYPTED);
     std::vector<uint8_t> pssh;
     std::string kid;
