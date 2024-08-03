xbmc/inputstream.adaptive

commit 9f711935f10c6eb71bfc30ae9110690fd58f54ff

Fix wrong Dash KID data format and cleanups

src/decrypters/Helpers.h
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace DRM
 {
@@ -45,5 +46,30 @@ std::string GenerateUrlDomainHash(std::string_view url);
  * \return The System ID, otherwise empty if fails.
  */
 std::string UrnToSystemId(std::string_view urn);
+
+/*!
+ * \brief Convert a hexdecimal KeyId of 32 chars to 16 bytes.
+ * \param kidStr The hexdecimal KeyId
+ * \return KeyId as bytes, otherwise empty if fails.
+ */
+std::vector<uint8_t> ConvertKidStrToBytes(std::string_view kidStr);
+
+/*!
+ * \brief Convert a KeyId of 16 bytes to a KeyId UUID format.
+ * \param kidStr The hexdecimal KeyId
+ * \return The KeyId UUID, otherwise empty if fails.
+ */
+std::string ConvertKidBytesToUUID(std::vector<uint8_t> kid);
+
+/*!
+ * \brief Convert a PlayReady KeyId of 16 bytes to a Widevine KeyId.
+ * \param kid The PlayReady KeyId
+ * \return The Widevine KeyId, otherwise empty if fails.
+ */
+std::vector<uint8_t> ConvertPrKidtoWvKid(std::vector<uint8_t> kid);
+
+bool CreateISMlicense(std::string_view kidStr,
+                      std::string_view licenseData,
+                      std::vector<uint8_t>& initData);
 
 }; // namespace DRM

src/Session.cpp
@@ -308,11 +308,11 @@ bool CSession::PreInitializeDRM(std::string& challengeB64,
   initData = BASE64::Decode(psshData);
 
   // Decode the provided KID
-  std::string decKid{BASE64::DecodeToStr(kidData)};
+  const std::vector<uint8_t> decKid = BASE64::Decode(kidData);
 
   CCdmSession& session(m_cdmSessions[1]);
 
-  std::string hexKid{StringUtils::ToHexadecimal(decKid)};
+  std::string hexKid{STRING::ToHexadecimal(decKid)};
   LOG::LogF(LOGDEBUG, "Initializing session with KID: %s", hexKid.c_str());
 
   if (m_decrypter && (session.m_cencSingleSampleDecrypter =
@@ -401,7 +401,7 @@ bool CSession::InitializeDRM(bool addDefaultKID /* = false */)
             LOG::Log(LOGDEBUG, "License data: Create Widevine PSSH for SmoothStreaming, based on "
                                "license data property");
           }
-          CreateISMlicense(sessionPsshset.defaultKID_, licenseData, initData);
+          DRM::CreateISMlicense(sessionPsshset.defaultKID_, licenseData, initData);
         }
         else if (licenseType == "com.microsoft.playready")
         {
@@ -443,7 +443,7 @@ bool CSession::InitializeDRM(bool addDefaultKID /* = false */)
       }
 
       CCdmSession& session{m_cdmSessions[ses]};
-      std::string defaultKid = sessionPsshset.defaultKID_;
+      const std::vector<uint8_t> defaultKid = DRM::ConvertKidStrToBytes(sessionPsshset.defaultKID_);
 
       if (addDefaultKID && ses == 1 && session.m_cencSingleSampleDecrypter)
       {
@@ -456,8 +456,7 @@ bool CSession::InitializeDRM(bool addDefaultKID /* = false */)
 
       if (m_decrypter && !defaultKid.empty())
       {
-        LOG::Log(LOGDEBUG, "Initializing stream with KID: %s",
-                 STRING::ToHexadecimal(defaultKid).c_str());
+        LOG::Log(LOGDEBUG, "Initializing stream with KID: %s", sessionPsshset.defaultKID_.c_str());
 
         for (size_t i{1}; i < ses; ++i)
         {
@@ -1490,8 +1489,7 @@ bool CSession::ExtractStreamProtectionData(PLAYLIST::CPeriod::PSSHSet& sessionPs
                   AP4_DYNAMIC_CAST(AP4_TencAtom, schi->GetChild(AP4_ATOM_TYPE_TENC, 0)) };
               if (tenc)
               {
-                sessionPsshset.defaultKID_ =
-                  std::string(reinterpret_cast<const char*>(tenc->GetDefaultKid()), 16);
+                sessionPsshset.defaultKID_ = STRING::ToHexadecimal(tenc->GetDefaultKid(), 16);
               }
               else
               {
@@ -1500,8 +1498,7 @@ bool CSession::ExtractStreamProtectionData(PLAYLIST::CPeriod::PSSHSet& sessionPs
                                      schi->GetChild(AP4_UUID_PIFF_TRACK_ENCRYPTION_ATOM, 0)) };
                 if (piff)
                 {
-                  sessionPsshset.defaultKID_ =
-                    std::string(reinterpret_cast<const char*>(piff->GetDefaultKid()), 16);
+                  sessionPsshset.defaultKID_ = STRING::ToHexadecimal(piff->GetDefaultKid(), 16);
                 }
               }
             }

src/common/AdaptiveDecrypter.h
@@ -24,7 +24,7 @@ public:
   /*! \brief Add a Key ID to the current session
    *  \param keyId The KID
    */
-  virtual void AddKeyId(std::string_view keyId)
+  virtual void AddKeyId(const std::vector<uint8_t>& keyId)
   {
     throw std::logic_error("AddKeyId method not implemented.");
   };
@@ -32,7 +32,7 @@ public:
   /*! \brief Set a Key ID as default
    *  \param keyId The KID
    */
-  virtual void SetDefaultKeyId(std::string_view keyId)
+  virtual void SetDefaultKeyId(const std::vector<uint8_t>& keyId)
   {
     throw std::logic_error("SetDefaultKeyId method not implemented.");
   };

src/decrypters/Helpers.cpp
@@ -8,6 +8,7 @@
 
 #include "Helpers.h"
 
+#include "utils/Base64Utils.h"
 #include "utils/DigestMD5Utils.h"
 #include "utils/StringUtils.h"
 #include "utils/UrlUtils.h"
@@ -65,4 +66,133 @@ bool DRM::IsKeySystemSupported(std::string_view keySystem)
   return keySystem == DRM::KS_NONE || keySystem == DRM::KS_WIDEVINE ||
     keySystem == DRM::KS_PLAYREADY || keySystem == DRM::KS_WISEPLAY ||
     keySystem == DRM::KS_CLEARKEY;
+}
+
+std::vector<uint8_t> DRM::ConvertKidStrToBytes(std::string_view kidStr)
+{
+  if (kidStr.size() != 32)
+  {
+    LOG::LogF(LOGERROR, "Cannot convert KID \"%s\" as bytes due to wrong size", kidStr.data());
+    return {};
+  }
+
+  std::vector<uint8_t> kidBytes(16, 0);
+  const char* kidPtr = kidStr.data();
+
+  for (size_t i{0}; i < 16; i++)
+  {
+    kidBytes[i] = STRING::ToHexNibble(*kidPtr) << 4;
+    kidPtr++;
+    kidBytes[i] |= STRING::ToHexNibble(*kidPtr);
+    kidPtr++;
+  }
+
+  return kidBytes;
+}
+
+std::string DRM::ConvertKidBytesToUUID(std::vector<uint8_t> kid)
+{
+  if (kid.size() != 16)
+    return {};
+
+  static char hexDigits[] = "0123456789abcdef";
+  std::string uuid;
+  for (size_t i{0}; i < 16; ++i)
+  {
+    if (i == 4 || i == 6 || i == 8 || i == 10)
+      uuid += '-';
+    uuid += hexDigits[kid[i] >> 4];
+    uuid += hexDigits[kid[i] & 15];
+  }
+  return uuid;
+}
+
+std::vector<uint8_t> DRM::ConvertPrKidtoWvKid(std::vector<uint8_t> kid)
+{
+  if (kid.size() != 16)
+    return {};
+
+  std::vector<uint8_t> remapped;
+  static const size_t remap[16] = {3, 2, 1, 0, 5, 4, 7, 6, 8, 9, 10, 11, 12, 13, 14, 15};
+  // Reordering bytes
+  for (size_t i{0}; i < 16; ++i)
+  {
+    remapped.emplace_back(kid[remap[i]]);
+  }
+  return remapped;
+}
+
+bool DRM::CreateISMlicense(std::string_view kidStr,
+                           std::string_view licenseData,
+                           std::vector<uint8_t>& initData)
+{
+  std::vector<uint8_t> kidBytes = ConvertKidStrToBytes(kidStr);
+
+  if (kidBytes.size() != 16 || licenseData.empty())
+  {
+    initData.clear();
+    return false;
+  }
+
+  std::string decLicData = BASE64::DecodeToStr(licenseData);
+  size_t origLicenseSize = decLicData.size();
+
+  const uint8_t* kid{reinterpret_cast<const uint8_t*>(std::strstr(decLicData.data(), "{KID}"))};
+  const uint8_t* uuid{reinterpret_cast<const uint8_t*>(std::strstr(decLicData.data(), "{UUID}"))};
+  uint8_t* decLicDataUint = reinterpret_cast<uint8_t*>(decLicData.data());
+
+  size_t license_size = uuid ? origLicenseSize + 36 - 6 : origLicenseSize;
+
+  //Build up proto header
+  initData.resize(512);
+  uint8_t* protoptr(initData.data());
+  if (kid)
+  {
+    if (uuid && uuid < kid)
+      return false;
+    license_size -= 5; //Remove sizeof(placeholder)
+    std::memcpy(protoptr, decLicDataUint, kid - decLicDataUint);
+    protoptr += kid - decLicDataUint;
+    license_size -= static_cast<size_t>(kid - decLicDataUint);
+    kid += 5;
+    origLicenseSize -= kid - decLicDataUint;
+  }
+  else
+    kid = decLicDataUint;
+
+  *protoptr++ = 18; //id=16>>3=2, type=2(flexlen)
+  *protoptr++ = 16; //length of key
+  std::memcpy(protoptr, kidBytes.data(), 16);
+  protoptr += 16;
+
+  *protoptr++ = 34; //id=32>>3=4, type=2(flexlen)
+  do
+  {
+    *protoptr++ = static_cast<uint8_t>(license_size & 127);
+    license_size >>= 7;
+    if (license_size)
+      *(protoptr - 1) |= 128;
+    else
+      break;
+  } while (1);
+  if (uuid)
+  {
+    std::memcpy(protoptr, kid, uuid - kid);
+    protoptr += uuid - kid;
+
+    std::string uuidKid{ConvertKidBytesToUUID(kidBytes)};
+    protoptr = reinterpret_cast<uint8_t*>(uuidKid.data());
+
+    size_t sizeleft = origLicenseSize - ((uuid - kid) + 6);
+    std::memcpy(protoptr, uuid + 6, sizeleft);
+    protoptr += sizeleft;
+  }
+  else
+  {
+    std::memcpy(protoptr, kid, origLicenseSize);
+    protoptr += origLicenseSize;
+  }
+  initData.resize(protoptr - initData.data());
+
+  return true;
 }

src/decrypters/IDecrypter.h
@@ -88,7 +88,7 @@ public:
   virtual Adaptive_CencSingleSampleDecrypter* CreateSingleSampleDecrypter(
       std::vector<uint8_t>& initData,
       std::string_view optionalKeyParameter,
-      std::string_view defaultKeyId,
+      const std::vector<uint8_t>& defaultKeyId,
       std::string_view licenseUrl,
       bool skipSessionMessage,
       CryptoMode cryptoMode) = 0;
@@ -107,7 +107,7 @@ public:
    * \param caps The capabilities object to be populated
    */
   virtual void GetCapabilities(Adaptive_CencSingleSampleDecrypter* decrypter,
-                               std::string_view keyId,
+                               const std::vector<uint8_t>& keyId,
                                uint32_t media,
                                DecrypterCapabilites& caps) = 0;
 
@@ -118,7 +118,7 @@ public:
    * \return True if the KeyID has a license otherwise false
    */
   virtual bool HasLicenseKey(Adaptive_CencSingleSampleDecrypter* decrypter,
-                             std::string_view keyId) = 0;
+                             const std::vector<uint8_t>& keyId) = 0;
 
   /**
    * \brief Check if the decrypter has been initialised (OpenDRMSystem called)

src/decrypters/clearkey/ClearKeyCencSingleSampleDecrypter.cpp
@@ -26,7 +26,7 @@
 using namespace UTILS;
 
 CClearKeyCencSingleSampleDecrypter::CClearKeyCencSingleSampleDecrypter(
-    std::string_view licenseUrl, std::string_view defaultKeyId, CClearKeyDecrypter* host)
+    std::string_view licenseUrl, const std::vector<uint8_t>& defaultKeyId, CClearKeyDecrypter* host)
   : m_host(host)
 {
   if (licenseUrl.empty())
@@ -76,7 +76,7 @@ CClearKeyCencSingleSampleDecrypter::CClearKeyCencSingleSampleDecrypter(
     return;
   }
 
-  const std::string b64DefaultKeyId = UTILS::BASE64::Encode(defaultKeyId.data());
+  const std::string b64DefaultKeyId = UTILS::BASE64::Encode(defaultKeyId);
   if (!STRING::KeyExists(m_keyPairs, b64DefaultKeyId))
   {
     LOG::LogF(LOGERROR, "Key not found on license server response");
@@ -96,7 +96,7 @@ CClearKeyCencSingleSampleDecrypter::CClearKeyCencSingleSampleDecrypter(
 
 CClearKeyCencSingleSampleDecrypter::CClearKeyCencSingleSampleDecrypter(
     const std::vector<uint8_t>& initData,
-    std::string_view defaultKeyId,
+    const std::vector<uint8_t>& defaultKeyId,
     const std::map<std::string, std::string>& keys,
     CClearKeyDecrypter* host)
   : m_host(host)
@@ -124,17 +124,17 @@ CClearKeyCencSingleSampleDecrypter::CClearKeyCencSingleSampleDecrypter(
   AddSessionKey(defaultKeyId);
 }
 
-void CClearKeyCencSingleSampleDecrypter::AddSessionKey(std::string_view keyId)
+void CClearKeyCencSingleSampleDecrypter::AddSessionKey(const std::vector<uint8_t>& keyId)
 {
   if (std::find(m_keyIds.begin(), m_keyIds.end(), keyId) == m_keyIds.end())
-    m_keyIds.push_back(std::string(keyId));
+    m_keyIds.emplace_back(keyId);
 }
 
-bool CClearKeyCencSingleSampleDecrypter::HasKeyId(std::string_view keyid)
+bool CClearKeyCencSingleSampleDecrypter::HasKeyId(const std::vector<uint8_t>& keyid)
 {
   if (!keyid.empty())
   {
-    for (const std::string& key : m_keyIds)
+    for (const std::vector<uint8_t>& key : m_keyIds)
     {
       if (key == keyid)
         return true;
@@ -161,7 +161,8 @@ AP4_Result CClearKeyCencSingleSampleDecrypter::DecryptSampleData(
                           bytes_of_encrypted_data);
 }
 
-std::string CClearKeyCencSingleSampleDecrypter::CreateLicenseRequest(std::string_view defaultKeyId)
+std::string CClearKeyCencSingleSampleDecrypter::CreateLicenseRequest(
+    const std::vector<uint8_t>& defaultKeyId)
 {
   // github.com/Dash-Industry-Forum/ClearKey-Content-Protection/blob/master/README.md
   /* Expected JSON structure for license request:
@@ -171,9 +172,8 @@ std::string CClearKeyCencSingleSampleDecrypter::CreateLicenseRequest(std::string
    *     ]
    * "type":"temporary" }
    */
-  std::string kid{defaultKeyId};
-  UTILS::STRING::ReplaceAll(kid, "-", "");
-  std::string b64Kid = UTILS::BASE64::Encode(kid);
+
+  std::string b64Kid = UTILS::BASE64::Encode(defaultKeyId);
   UTILS::STRING::ReplaceAll(b64Kid, "=", "");
 
   rapidjson::Document jDoc;

src/decrypters/clearkey/ClearKeyCencSingleSampleDecrypter.h
@@ -17,15 +17,15 @@ class CClearKeyCencSingleSampleDecrypter : public Adaptive_CencSingleSampleDecry
 {
 public:
   CClearKeyCencSingleSampleDecrypter(std::string_view licenseUrl,
-                                     std::string_view defaultKeyId,
+                                     const std::vector<uint8_t>& defaultKeyId,
                                      CClearKeyDecrypter* host);
   CClearKeyCencSingleSampleDecrypter(const std::vector<uint8_t>& initdata,
-                                     std::string_view defaultKeyId,
+                                     const std::vector<uint8_t>& defaultKeyId,
                                      const std::map<std::string, std::string>& keys,
                                      CClearKeyDecrypter* host);
   virtual ~CClearKeyCencSingleSampleDecrypter(){};
-  void AddSessionKey(std::string_view keyId);
-  bool HasKeyId(std::string_view keyid);
+  void AddSessionKey(const std::vector<uint8_t>& keyId);
+  bool HasKeyId(const std::vector<uint8_t>& keyid);
   virtual AP4_Result SetFragmentInfo(AP4_UI32 pool_id,
                                      const std::vector<uint8_t>& key,
                                      const AP4_UI08 nal_length_size,
@@ -42,17 +42,17 @@ public:
                                        unsigned int subsample_count,
                                        const AP4_UI16* bytes_of_cleartext_data,
                                        const AP4_UI32* bytes_of_encrypted_data) override;
-  std::string CreateLicenseRequest(std::string_view defaultKeyId);
+  std::string CreateLicenseRequest(const std::vector<uint8_t>& defaultKeyId);
   bool ParseLicenseResponse(std::string data);
-  void SetDefaultKeyId(std::string_view keyId) override{};
-  void AddKeyId(std::string_view keyId) override{};
+  void SetDefaultKeyId(const std::vector<uint8_t>& keyId) override{};
+  void AddKeyId(const std::vector<uint8_t>& keyId) override{};
   bool HasKeys() { return !m_keyIds.empty(); }
 
 private:
   AP4_CencSingleSampleDecrypter* m_singleSampleDecrypter{nullptr};
   std::string m_strSession;
   std::string m_licenceDefaultKeyId;
-  std::vector<std::string> m_keyIds;
+  std::vector<std::vector<uint8_t>> m_keyIds;
   std::map<std::string, std::string> m_keyPairs;
   CClearKeyDecrypter* m_host;
 };

src/decrypters/clearkey/ClearKeyDecrypter.cpp
@@ -34,7 +34,7 @@ bool CClearKeyDecrypter::OpenDRMSystem(std::string_view licenseURL,
 Adaptive_CencSingleSampleDecrypter* CClearKeyDecrypter::CreateSingleSampleDecrypter(
     std::vector<uint8_t>& initData,
     std::string_view optionalKeyParameter,
-    std::string_view defaultkeyid,
+    const std::vector<uint8_t>& defaultkeyid,
     std::string_view licenseUrl,
     bool skipSessionMessage,
     CryptoMode cryptoMode)
@@ -68,7 +68,7 @@ void CClearKeyDecrypter::DestroySingleSampleDecrypter(Adaptive_CencSingleSampleD
 }
 
 bool CClearKeyDecrypter::HasLicenseKey(Adaptive_CencSingleSampleDecrypter* decrypter,
-                                       std::string_view keyId)
+                                       const std::vector<uint8_t>& keyId)
 {
   if (decrypter)
     return static_cast<CClearKeyCencSingleSampleDecrypter*>(decrypter)->HasKeyId(keyId);

commit 6e8f0388725cd0d4781daf27b2d47934505a9cd1

include <cstdint> for uint8_t
fixes build with gcc >= 14

Fixes: 9f711935f10c ("Fix wrong Dash KID data format and cleanups")

src/decrypters/Helpers.h
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <string_view>
 #include <vector>